14408c37f9acbc63c363808f16f68612c126ab18
Bhisa <52441052+Dorky-PittTa@users.noreply.github.com>
2024-09-10 12:09:42 +0700

    Add normal tests to RationalTest.java

--- src/test/java/RationalTest.java
+++ src/test/java/RationalTest.java
@@ -34,6 +34,58 @@ public class RationalTest {
         Assert.assertEquals(4, r1.denominator);
     }
 
+    @Test
+    public void testAddSimple(){
+        r1.numerator = 1;
+        r1.denominator = 2;
+        r2.numerator = 2;
+        r2.denominator = 8;
+        r1.add(r2);
+        Assert.assertEquals(3, r1.numerator);
+        Assert.assertEquals(4, r1.denominator);
+    }
+
+    @Test
+    public void testSub(){
+        r1.numerator = 1;
+        r1.denominator = 2;
+        r2.numerator = 1;
+        r2.denominator = 4;
+        r1.subtract(r2);
+        Assert.assertEquals(1, r1.numerator);
+        Assert.assertEquals(4, r1.denominator);
+    }
+
+    @Test
+    public void testMulti(){
+        r1.numerator = 1;
+        r1.denominator = 2;
+        r2.numerator = 2;
+        r2.denominator = 3;
+        r1.multiply(r2);
+        Assert.assertEquals(1, r1.numerator);
+        Assert.assertEquals(3, r1.denominator);
+    }
+
+    @Test
+    public void testDivide(){
+        r1.numerator = 2;
+        r1.denominator = 4;
+        r2.numerator = 4;
+        r2.denominator = 8;
+        r1.divide(r2);
+        Assert.assertEquals(1, r1.numerator);
+        Assert.assertEquals(1, r1.denominator);
+    }
+
+    @Test
+    public void testEqual(){
+        r1.numerator = 2;
+        r1.denominator = 4;
+        r2.numerator = 4;
+        r2.denominator = 8;
+    }
+
     /***
      * This method will be called every time after the other @Test method
      * is called.
